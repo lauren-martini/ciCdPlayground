acac6c413077f24fac90e5f7bb881b9918b5d2f6
Lauren Martini <lauren.martini.ext@bestsecret.com>
2024-11-15 14:13:47 +0100

    Update text content for introduction

--- src/main.js
+++ src/main.js
@@ -4,25 +4,25 @@ new App({
 	target: document.body,
 	props: {
 		// What's your name?
-		name: 'James Butler',
+		name: 'Lauren Martini',
 		// In the following fiels you can either give a single string, 
 		// or an array of bullet points
 		
 		// What do you associate with the term 'CI/CD'?
 		associations: [
-			'Ehm, what?'
+			'GitLab CI, GitHub Actions'
 		],
 		// Which CI/CD tools do you use in your project?
-		tools: 'Jenkins, maybe?',
+		tools: 'GitLab CI, Jenkins',
 		// What do you want to learn in this workshop? 
 		expectations: [
-			'what you are talking about'
+			'CI/CD other than GitLab CI'
 		],
 		// What do you like to do when you're not coding?
 		hobbies: [
-			'play chess', 
-			'do skydiving', 
-			'do my taxes'
+			'violin',
+			'baking',
+			'video games'
 		]
 	}
 });
